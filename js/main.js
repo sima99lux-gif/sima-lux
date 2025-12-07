01685892f9be03e5e7b0786a89a520dfeffa6be0
sima99lux-gif <sima99lux@gmail.com>
2025-12-07 08:25:03 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,67 @@
+(function($) {
+
+	"use strict";	
+
+  	$(".main-menu a").click(function(){
+		var id =  $(this).attr('class');
+		id = id.split('-');
+		$('a.active').removeClass('active');
+    	$(this).addClass('active');
+		$("#menu-container .content").slideUp('slow');
+		$("#menu-container #menu-"+id[1]).slideDown('slow');		
+		$("#menu-container .homepage").slideUp('slow');
+		return false;
+	});
+
+
+	$(".main-menu a.homebutton").click(function(){
+		$("#menu-container .content").slideUp('slow');
+		$("#menu-container .homepage").slideDown('slow');
+		$(".logo-top-margin").animate({marginLeft:'35%'}, "slow");
+		$(".logo-top-margin").animate({marginTop:'30px'}, "slow");
+		return false;
+	});
+
+	$(".main-menu a.aboutbutton").click(function(){
+		$("#menu-container .content").slideUp('slow');
+		$("#menu-container .about-section").slideDown('slow');
+		$(".logo-top-margin").animate({marginTop:'0'}, "slow");
+		$(".logo-top-margin").animate({marginLeft:'0'}, "slow");
+		return false;
+	});
+
+	$(".main-menu a.projectbutton").click(function(){
+		$("#menu-container .content").slideUp('slow');
+		$("#menu-container .gallery-section").slideDown('slow');
+		$(".logo-top-margin").animate({marginTop:'0'}, "slow");
+		$(".logo-top-margin").animate({marginLeft:'0'}, "slow");
+		return false;
+	});
+
+	$(".main-menu a.contactbutton").click(function(){
+		$("#menu-container .content").fadeOut();
+		$("#menu-container .contact-section").slideDown('slow');
+		$(".logo-top-margin").animate({marginTop:'0'}, "slow");
+		$(".logo-top-margin").animate({marginLeft:'0'}, "slow");
+		return false;
+	});
+
+	$('.toggle-menu').click(function(){
+        $('.show-menu').stop(true,true).slideToggle();
+        return false;
+    });
+
+    $('.show-menu a').click(function() {
+    	$('.show-menu').fadeOut('slow');
+    });
+
+	$(document).ready(function(){
+    $(".welcome-text p").each(function(){
+        var text = $(this).html();
+        text = text.replace(/\.\s*/g, '.<br>'); // بعد كل نقطة إضافة <br>
+        $(this).html(text);
+    });
+});
+
+	
+})(jQuery);
